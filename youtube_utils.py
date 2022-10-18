ac5ba92f3297d6ecf2d5a444440d557e74f281df
Eddie Mattia <eddie@outerbounds.com>
2022-10-18 17:53:07 -0500

    add w2v

--- youtube_utils.py
+++ youtube_utils.py
@@ -23,20 +23,6 @@ def filenameify(value, allow_unicode=False):
     value = re.sub(r'[^\w\s-]', '', value.lower())
     return re.sub(r'[-\s]+', '_', value).strip('-_')
 
-def get_default_device():
-    
-    if torch.cuda.is_available():
-        return torch.device("cuda")
-    
-    # elif torch.backends.mps.is_built() :
-    #     # https://pytorch.org/blog/introducing-accelerated-pytorch-training-on-mac/
-    #     _p = platform()
-    #     if _p.startswith('macOS') and float(_p.split('-')[1]) >= 12.3:
-    #         return torch.device("mps")
-    # this would be nice. still breaks for me :'\
-    
-    return torch.device("cpu")
-
 class YouTubeTranscriptionTask(BaseModel):
     url: str
     filename: str
@@ -48,7 +34,6 @@ class YouTubeTranscriptionTask(BaseModel):
     description: str
     id: str
     publish_date: dt.datetime
-    transcription_text: str = ''
 
 def make_task(video_url, model_type):
     video = YouTube(video_url)
@@ -64,26 +49,3 @@ def make_task(video_url, model_type):
         'id': video.video_id,
         'publish_date': video.publish_date
     })
-
-def transcribe_video(transcription_task, output_path = './youtube-audio-files/', quiet=False):
-    """
-    Extract the audio from the YouTube watch `url` in the `transcription_task`. 
-    The audio will be saved locally to `output_path/transcription_task.filename`. 
-    Load OpenAI's `whisper` model on `device` and use its automatic transcription capability.
-        Note: there are `result` we are not returning that could be added to the flow versioning.
-    """
-    import whisper
-    if not quiet:
-        print("Extracting audio from video at {}...".format(transcription_task.url))
-    audio = YouTube(transcription_task.url).streams.get_audio_only()
-    audio.download(output_path = output_path, filename = transcription_task.filename)
-    audio_filename = output_path + transcription_task.filename
-    device = torch.device('cpu')
-    if not quiet:
-        print("Loading {} model on {}...".format(transcription_task.model_type, device))
-    model = whisper.load_model(transcription_task.model_type, device = device)
-    if not quiet:
-        print("Model loaded successfully...")
-        print("Transcribing {}...".format(audio_filename))
-    result = model.transcribe(audio_filename)
-    return result['text']
--- youtube_video_transcriber.py
+++ youtube_video_transcriber.py
@@ -1,111 +1,143 @@
+from nlp_utils import Mixin
 from metaflow import FlowSpec, step, Parameter, batch, card, current
 import os
 from dotenv import load_dotenv
 load_dotenv('.env')
 
-class YouTubeVideoTranscription(FlowSpec):
+class YouTubeVideoTranscription(FlowSpec, Mixin):
 
-    url = Parameter(
+    URL = Parameter(
         'url', type = str, 
         default = os.getenv('DEFAULT_VIDEO_URL', 'https://www.youtube.com/watch?v=OH0Y_DUZu4Y'),
         help = """The watch url of a YouTube video (starting with https://www.youtube.com/watch),
                   or a url to a Playlist (starting with https://www.youtube.com/playlist)."""
     )
-    url_filename = Parameter(
+    
+    URL_FILENAME = Parameter(
         'urls', type = str, 
         default = os.getenv('MANY_URL_FILENAME_DEFAULT', ''),
         help = "A file containing a list of watch urls for YouTube videos."
     )
-    model_type = Parameter(
+    
+    MODEL_TYPE = Parameter(
         'model', type = str, 
         default = os.getenv('DEFAULT_MODEL_TYPE', 'tiny'),
         help = """The size of the Whisper variant. 
                   See the current options at https://github.com/openai/whisper/blob/main/whisper/__init__.py#L17-L27"""
     )
-    
-    make_wordcloud = Parameter('wc', type = bool, default = True)
  
+    TITLE = Parameter(
+        'title', type = str,
+        default = 'Metaflow',
+        help = "The title to be displayed at the top the post processing step's card."
+    )
+    
+    MIN_COUNT_W2V = Parameter(
+        'min-ct', type = int, default = 5,
+        help = "The title to be displayed at the top the post processing step's card."
+    )
+    
+    AUDIO_OUTPUT = Parameter('audio-dst', type = str, default = './youtube-audio-files/')
+
     @step
     def start(self):
 
-        from pytube import Playlist
+        from pytube import Playlist, YouTube
+        from pytube.exceptions import LiveStreamError
         from youtube_utils import make_task
 
-        if self.url_filename == os.getenv('MANY_URL_FILENAME_DEFAULT', ''):
+        if self.URL_FILENAME == os.getenv('MANY_URL_FILENAME_DEFAULT', ''):
 
             # single video watch urls look like:
             # 'https://www.youtube.com/watch?v=<VIDEO ID>'
-            if self.url.startswith('https://www.youtube.com/watch'):
-                self.pending_transcription_task = [
-                    make_task(self.url, self.model_type)
+            if self.URL.startswith('https://www.youtube.com/watch'):
+                _pending_transcription_task = [
+                    make_task(self.URL, self.MODEL_TYPE)
                 ]
 
             # playlist urls look like: 
             # 'https://www.youtube.com/playlist?list=<PLAYLIST ID>'
-            elif self.url.startswith('https://www.youtube.com/playlist'):
-                self.pending_transcription_task = [
-                    make_task(video_url, self.model_type)
-                    for video_url in Playlist(self.url)
+            elif self.URL.startswith('https://www.youtube.com/playlist'):
+                _pending_transcription_task = [
+                    make_task(video_url, self.MODEL_TYPE)
+                    for video_url in Playlist(self.URL)
                 ]
               
         else: # user passed a list of urls
-            self.pending_transcription_task = []
-            with open(self.url_filename, 'r') as file:
+            _pending_transcription_task = []
+            with open(self.URL_FILENAME, 'r') as file:
                 for url_line in file:
                     if url_line.strip().startswith('https://www.youtube.com/playlist'):
-                        self.pending_transcription_task.extend([
-                            make_task(video_url, self.model_type)
+                        _pending_transcription_task.extend([
+                            make_task(video_url, self.MODEL_TYPE)
                             for video_url in Playlist(url_line.strip())
                         ])
                     else:
-                        self.pending_transcription_task.append(
-                            make_task(url_line.strip(), self.model_type)
+                        _pending_transcription_task.append(
+                            make_task(url_line.strip(), self.MODEL_TYPE)
                         )
-
+        
+        # data validation
+        _live_streams = []
+        for _transcription_task in _pending_transcription_task:
+            try:
+                audio = YouTube(_transcription_task.url).streams.get_audio_only()
+                audio.download(output_path = self.AUDIO_OUTPUT, filename = _transcription_task.filename)
+                audio_filename = self.AUDIO_OUTPUT + _transcription_task.filename
+            except LiveStreamError:
+                print("{} is a coming live stream, removing it from the pending transcription list.".format(_transcription_task.url))
+                _live_streams.append(_transcription_task)
+            
+        self.pending_transcription_task = []
+        for _task in _pending_transcription_task:
+            if _task not in _live_streams:
+                self.pending_transcription_task.append(_task)
+        
         self.next(self.transcribe, foreach='pending_transcription_task')
 
-    # @batch(
-    #     cpu = 8, # gpu = 1, 
-    #     memory = int(os.getenv('MEMORY_REQUIRED', '10000')),
-    #     image = os.getenv('CPU_IMAGE', 'eddieob/whisper-cpu:latest'),
-    #     queue = os.getenv('BATCH_QUEUE_CPU')
-    # )
+    @batch(
+        cpu = 8, gpu = 1,
+        memory = int(os.getenv('MEMORY_REQUIRED', '12000')),
+        image = os.getenv('GPU_IMAGE', 'eddieob/whisper-gpu:latest'),
+        queue = os.getenv('BATCH_QUEUE_GPU')
+    )
     @step
     def transcribe(self):
-        from youtube_utils import transcribe_video
         self.transcription = self.input
-        self.transcription.transcription_text = transcribe_video(self.transcription)
+        self.transcription_text = self.transcribe_video(self.transcription)
         self.next(self.postprocess)
 
     @card
     @step
-    def postprocess(self, parent_steps):
+    def postprocess(self, transcribe_steps):
         import pandas as pd
-        self.results = pd.DataFrame([_step.transcription.dict() for _step in parent_steps])
+        from metaflow.cards import Image, Table, Markdown
+        import matplotlib.pyplot as plt
         
-        if self.make_wordcloud:
-            from nlp_utils import aggregate_stopwords
-            from metaflow.cards import Image
-            import matplotlib.pyplot as plt
-            from scipy.ndimage import gaussian_gradient_magnitude
-            from wordcloud import WordCloud, ImageColorGenerator
-            all_text = " ".join(
-                v.strip() 
-                for v in self.results.transcription_text.values
-            )
-            wordcloud = WordCloud(
-                max_words = 100, 
-                max_font_size=40, 
-                background_color='white',
-                stopwords = aggregate_stopwords()
-            ).generate(all_text)
-
-            fig, ax = plt.subplots(1,1)
-            plt.axis("off")
-            ax.imshow(wordcloud, interpolation="bilinear")
-            current.card.append(Image.from_matplotlib(fig))
-            
+        # gather documents
+        self.documents = []
+        task_data = []
+        for _step in transcribe_steps:
+            task_data.append(_step.transcription.dict())
+            self.documents.append(_step.transcription_text)
+        self.results = pd.DataFrame(task_data)
+        _joined_document = " ".join(document.strip() for document in self.documents)
         
+        # create visualizations
+        self.fig, ax = plt.subplots(1, 2, figsize=(16,8))
+        self.fig.suptitle("{} - Wordcloud and Word2vec Embedding".format(self.TITLE), fontsize=32)
+        _ = self.get_wordcloud_figure(document=_joined_document, fig=self.fig, ax=ax[0])
+        _ = self.get_w2v_figure(document=_joined_document, fig=self.fig, ax=ax[1])
+        current.card.append(Table([[Image.from_matplotlib(self.fig)]]))
+        
+        # show transcription in Metaflow card
+        md = [Markdown("# Transcriptions from Whisper {}".format(self.MODEL_TYPE))]
+        for i, document in enumerate(self.documents):
+            md.extend([
+                Markdown("## *Video Name:* {}".format(task_data[i]['title'])),
+                Markdown(document)
+            ])
+        current.card.extend(md)
         self.next(self.end)
 
     @step
